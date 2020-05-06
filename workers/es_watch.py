3b5d7f838a3d086b7f2e1828521431be66133c08
Shrivu Shankar <shrivu1122@gmail.com>
2020-05-05 21:21:55 -0500

    🧰: Fixed Requirements, Added ES Watcher

--- workers/es_watch.py
+++ workers/es_watch.py
@@ -2,18 +2,26 @@ from stream.twitter import StreamTwitter
 from stream.tdameritrade import StreamTDA
 from stream.prs import StreamPRs
 from stream.news import StreamNews
-from notify.slack import slack_evt
+from articles import hash_sha1
+import pendulum
+import elasticsearch
 
 import nest_asyncio; nest_asyncio.apply()
-
-
-def on_event(evt):
-    print(evt)
 
 
 def main():
 
     print('Watching...')
+
+    es = elasticsearch.Elasticsearch()  
+
+    def on_event(evt):
+        print(evt)
+        evt['date'] = str(pendulum.now())
+        try:
+            es.create('index-evt', hash_sha1(str(evt)), evt)
+        except elasticsearch.exceptions.ConflictError:
+            pass
     
     strm_twitter = StreamTwitter()
     strm_twitter.on_event = on_event
