e4ae373b950aec4ffb2174b38d108ad398db3703
Shrivu Shankar <shrivu1122@gmail.com>
2020-05-20 18:08:37 -0500

    🕹️: Financials

---
+++ workers/stats/guru.py
@@ -0,0 +1,47 @@
+from . import MetaDataSource
+import subprocess
+import pendulum
+import tempfile
+import json
+import os
+import re
+
+
+def _exec_js(code):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        js_path = os.path.join(tmp_dir, 'tmp.js')
+        with open(js_path, 'w') as f:
+            f.write(code)
+        result = subprocess.run(['node', js_path], stdout=subprocess.PIPE)
+        out = str(result.stdout, 'utf-8')
+        data = json.loads(out)
+        return data
+
+
+def _flatten_guru(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        new_key = new_key.replace(' ', '_').replace('meidum', 'medium').lower()
+        if isinstance(v, dict):
+            items.extend(_flatten_guru(v, new_key, sep=sep).items())
+        elif isinstance(v, list) or 'display_name' in k or 'origin_key' in k:
+            pass
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+class Guru(MetaDataSource):
+
+    def __init__(self):
+        self.url = 'https://www.gurufocus.com'
+
+    async def read_financials(self, sym):
+        resp = await self._get('/stock/{}/summary'.format(sym))
+        js_script = re.search(r'window.__NUXT__=([\s\S]+?);<\/script>', resp).group(1)
+        raw_data = _exec_js('console.log(JSON.stringify(' + js_script + '.data[0].stock))')
+        raw_data.update(_exec_js('console.log(JSON.stringify(' + js_script + '.data[1].summaryView.data))'))
+        data = _flatten_guru(raw_data)
+        assert data['symbol'] == sym
+        return 'guru', sym, data
---
+++ workers/stream/guru.py
@@ -0,0 +1,50 @@
+from config import config
+from stats.guru import Guru
+from . import StreamPoll
+import asyncio
+import aiohttp
+
+
+CFG = config['watch']['guru']
+
+
+class StreamGuru(StreamPoll):
+
+    def __init__(self):
+        self.scraper = Guru()
+        self.hist = {}
+        self.stocks = CFG['stocks']
+        self.delay = CFG['delay']
+
+    async def get_polls(self):
+        polls = []
+        for stock in self.stocks:
+            async def scrap_stock():
+                return await self.scraper.read_financials(stock)
+            polls.append((self.scraper, scrap_stock, self.delay))
+        return polls
+
+    async def on_poll_data(self, source, sym, fin_data, emit_events=True, **kwargs):
+        prev = self.hist.get(sym)
+        self.hist[sym] = fin_data.copy()
+        if prev is not None:
+            diff_data = {}
+            for key, og_val in prev.items():
+                new_val = fin_data.get(key, og_val)
+                if type(og_val) != type(new_val) or og_val == new_val:
+                    continue
+                if type(new_val) in [int, float]:
+                    diff = new_val - og_val
+                    if og_val != 0:
+                        diffp = diff / og_val
+                    else:
+                        diffp = 0
+                    diff_data[key + '_diff'] = diff
+                    diff_data[key + '_diffpercent'] = diffp
+                else:
+                    diff_data[key + '_new'] = new_val
+                    diff_data[key + '_prev'] = og_val
+            diff_data.update(dict(source=source, type='financials', name='guru-diff', symbols=[sym]))
+            self.on_event(diff_data)
+        fin_data.update(dict(source=source, type='financials', name='guru-spot', symbols=[sym]))
+        self.on_event(fin_data)
